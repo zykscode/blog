9c63b99a466102d52f10456039d601a2a53b060b
zykscode <111462249+zykscode@users.noreply.github.com>
2022-12-01 02:12:19 +0100

    feat: 2nd commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
-import '#/styles/globals.css'
+import '../styles/globals.css'
+import '../styles/styles.scss'
 export default function RootLayout({
   children,
 }: {
@@ -11,7 +12,16 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body className='bg-green-400 h-10'>{children}</body>
+      <body className='bg-green-400 h-10'>
+        <nav className='nav' >
+          <ul >
+            <li className='bg-pink-400'>hwlle</li>
+            <li>hwlle</li>
+            <li>hwlle</li>
+            <li>hwlle</li>
+          </ul>
+        </nav>
+        {children}</body>
     </html>
   )
 }
