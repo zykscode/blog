e6e249ce57e870d0faae61fde4d4062542aead18
zykscode <111462249+zykscode@users.noreply.github.com>
2023-01-26 10:49:16 +0100

    feat: finished 404 page

--- Layouts/ErrorLayout.tsx
+++ Layouts/ErrorLayout.tsx
@@ -7,7 +7,7 @@ function ErrorLayout({}) {
   const { theme} = useTheme()
   return (
     <div className="page-scroller ">
-    <section className="m-auto grid lg:grid-cols-2 min-h-3/4 rounded-2xl w-5/6 md:w-3/5 ">
+    <section className="m-auto bg-[var(--blueBackground)] grid min-h-3/4 rounded-2xl w-5/6 md:w-3/5 ">
     <FourZeroFour theme={theme}/>
     </section>
     <Footer />
